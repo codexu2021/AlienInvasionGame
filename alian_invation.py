a65d867a78c479bad1b9a93c381c01cba47a51d6
codexu2021 <82790256+codexu2021@users.noreply.github.com>
2021-07-26 21:22:58 +0900

    ゲーム画面の用意

--- alian_invation.py
+++ alian_invation.py
@@ -3,4 +3,20 @@ import pygame
 
 class AlienInvasaion:
 
-  def 
+  def __init__(self):
+    pygame.init()
+
+    self.screen = pygame.display.set_mode((1200,800))
+    pygame.display.set_caption("エイリアン侵略")
+
+  def run_game(self):
+    while True:
+      for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+          sys.exit()
+
+      pygame.display.flip()
+
+if __name__ == '__main__':
+  ai = AlienInvasaion()
+  ai.run_game()

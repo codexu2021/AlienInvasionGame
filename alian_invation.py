ffef0c0dff38712c89ddd1339e975f03f63de529
codexu2021 <82790256+codexu2021@users.noreply.github.com>
2021-07-26 21:50:57 +0900

    新しいファイルの追加 	new file:   .DS_Store 	new file:   __pycache__/setting.cpython-39.pyc 	modified:   alian_invation.py 	new file:   images/ship.bmp 	new file:   setting.py 	new file:   ship.py

--- alian_invation.py
+++ alian_invation.py
@@ -1,19 +1,27 @@
 import sys
 import pygame
 
+from setting import Settings
+
 class AlienInvasaion:
 
   def __init__(self):
     pygame.init()
+    self.settings = Settings()
 
-    self.screen = pygame.display.set_mode((1200,800))
+    self.screen = pygame.display.set_mode(
+      (self.settings.screen_width, self.settings.screen_height))
     pygame.display.set_caption("エイリアン侵略")
+
+    self.bg_color = (self.settings.bg_color)
 
   def run_game(self):
     while True:
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           sys.exit()
+
+      self.screen.fill(self.bg_color)
 
       pygame.display.flip()
 
